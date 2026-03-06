b1c652dd6b40b00fb3c67ee242ba1b5788ee7f1b
Vivekananda Bediya <vivekanandbediya006@gmail.com>
2026-03-06 08:16:04 +0530

    first commit

---
+++ src/data/books.js
@@ -0,0 +1,28 @@
+const books = [
+  {
+    id: 1,
+    title: "React Basics",
+    author: "John Smith",
+    category: "Programming",
+    description: "Learn React from scratch",
+    rating: 4
+  },
+  {
+    id: 2,
+    title: "Space Journey",
+    author: "Mark Lee",
+    category: "Sci-Fi",
+    description: "Adventure in space",
+    rating: 5
+  },
+  {
+    id: 3,
+    title: "Business Mind",
+    author: "Elon Ray",
+    category: "Non-Fiction",
+    description: "Business strategies",
+    rating: 4
+  }
+];
+
+export default books;
